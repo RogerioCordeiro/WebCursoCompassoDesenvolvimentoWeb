358fc08c204d2a1a874e498c65d5302c51d9d5d5
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-21 11:55:19 -0300

    Convertendo uma String para Numero - Seção 4 aula 14

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let firstName = prompt("Digite seu primeiro nome")
 let lastName = prompt("Digite seu último nome")
-let age = prompt("Digite sua idade")
-
+let age = parseInt(prompt("Digite sua idade"))  
+age = age + 2
 console.log('Meu nome é ' + firstName + ' ' + lastName)
 console.log('Eu tenho ' + age + ' de idade.') 
