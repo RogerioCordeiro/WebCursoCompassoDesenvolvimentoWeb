24059f437db41b4d0138e20e6d6e95c2126fedde
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-21 11:50:53 -0300

    Primeiro Commit Curso Compasso Desenvolvimento Web + Projetos

---
+++ main.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Digite seu primeiro nome")
+let lastName = prompt("Digite seu último nome")
+let age = prompt("Digite sua idade")
+
+console.log('Meu nome é ' + firstName + ' ' + lastName)
+console.log('Eu tenho ' + age + ' de idade.') 
