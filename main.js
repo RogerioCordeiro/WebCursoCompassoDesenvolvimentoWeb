9cd07b4f6b048adedb2f53c7bc776a8ea43766ea
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-21 12:07:28 -0300

    Aplicando estilos a pagina web aula16 secao5

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let firstName = prompt("Digite seu primeiro nome")
-let lastName = prompt("Digite seu último nome")
-let age = parseInt(prompt("Digite sua idade"))  
-const age2 = 2
-// Const - variavel não pode ser alterada 
-// age2 = 4
+// let firstName = prompt("Digite seu primeiro nome")
+// let lastName = prompt("Digite seu último nome")
+// let age = parseInt(prompt("Digite sua idade"))  
+// const age2 = 2
+// // Const - variavel não pode ser alterada 
+// // age2 = 4
 
-age = age + age2
-console.log('Meu nome é ' + firstName + ' ' + lastName)
-console.log('Eu tenho ' + age + ' de idade.') 
+// age = age + age2
+// console.log('Meu nome é ' + firstName + ' ' + lastName)
+// console.log('Eu tenho ' + age + ' de idade.') 
