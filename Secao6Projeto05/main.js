5da0a7ba2981ccc98c2f6d165013a2bc9f4299e9
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-21 12:36:51 -0300

    Secao6 Aulas 18, 19, 20 inserindo dados no html via javascript.

--- Secao6Projeto05/main.js
+++ Secao6Projeto05/main.js
@@ -1,10 +1,3 @@
-// let firstName = prompt("Digite seu primeiro nome")
-// let lastName = prompt("Digite seu último nome")
-// let age = parseInt(prompt("Digite sua idade"))  
-// const age2 = 2
-// // Const - variavel não pode ser alterada 
-// // age2 = 4
-
-// age = age + age2
-// console.log('Meu nome é ' + firstName + ' ' + lastName)
-// console.log('Eu tenho ' + age + ' de idade.') 
+let firstName = prompt("Digite seu nome")
+let greeting = 'Hello'
+document.getElementById("greeting").innerHTML = greeting + ' ' + firstName
