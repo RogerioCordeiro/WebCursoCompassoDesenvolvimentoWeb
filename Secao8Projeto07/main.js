1de06b7917b36f3725d12a97a841902967acf0bf
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-21 13:08:55 -0300

    Secao8 Aulas 24, 25, 26. Operadores aritmeticos em javascript. Conversor de C para F

---
+++ Secao8Projeto07/main.js
@@ -0,0 +1,21 @@
+// let a = 10
+// let b = 5
+// let c = 2.5
+
+// let resultado = a / b
+// console.log(resultado)
+// soma = +
+// divisão = /
+// multiplicação = *
+// subtração = -
+// resto da divisão = %
+// exponencial = **
+// ordem dos operadores () ** * e ou / depois + e ou - 
+let celsius = 0
+let fahrenheint = 0
+
+celsius = parseFloat(prompt('Digite a temperatura em Celsius: '))
+
+fahrenheint = celsius * 1.8 + 32
+
+alert("A temperatura em Fahrenheint é de " + fahrenheint.toFixed(2) + " graus")
