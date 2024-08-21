2ad59e5b05a1d8c17522bc146048b37c4dea867b
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-08-21 12:45:09 -0300

    Secao7 Aulas 21, 22, 23 Listas "Arrays" em javascript

---
+++ Secao7Projeto06/main.js
@@ -0,0 +1,11 @@
+let friends = ['Alan', 'Mark', 'Julia', 'Bella']
+console.log(friends)
+console.log(friends[1])
+friends[1] = "Maverick" // Replace
+console.log(friends)
+console.log(friends[1])
+friends[4] = 'Ana' // Add items
+console.log(friends)
+
+// Adcionar novos items basta informar o indice do novo item
+// para alterrar informar o indice e o novo valor
